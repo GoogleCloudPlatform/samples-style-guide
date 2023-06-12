dd541645dca7c685eaea0cbeba4d6567b5213d81
grayside <grayside@users.noreply.github.com>
2023-06-12 17:02:18 +0000

    stage: PR-125: 9fa5cb4672ff9e8ebdbcaa72b2ebb4c46d2a6305

---
+++ previews/PR-125/examples/go/imports.go
@@ -0,0 +1,11 @@
+package example
+
+// Region tags should start after the package, but before imports.
+// [START product_example]
+import "example.com/resource"
+
+func exampleSnippet() error {
+    // Snippet Content ...
+}
+
+// [END product_example]

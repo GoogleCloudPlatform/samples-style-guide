924d3e47a7c9aa110097748a64fa9e8d897b20fe
grayside <grayside@users.noreply.github.com>
2023-11-21 16:40:18 +0000

    stage: PR-146: 1712e405928f055b6388e6b13018ae2356622139

---
+++ previews/PR-146/examples/dotnet/Imports.cs
@@ -0,0 +1,8 @@
+// [START product_example]
+using System;
+
+public class ExampleSnippet
+{
+    // Snippet methods ...
+}
+// [END product_example]
---
+++ previews/PR-146/examples/php/imports.php
@@ -0,0 +1,7 @@
+// [START product_example]
+use Example\Resource
+
+function example_snippet() {
+    // Snippet Content ...
+}
+// [END product_example]

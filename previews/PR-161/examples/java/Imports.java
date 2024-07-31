464f8790244af0b6ff26d596c1dbd4eaab86ded6
glasnt <glasnt@users.noreply.github.com>
2024-07-31 23:42:01 +0000

    stage: PR-161: 5edff43965d2c92509709485875462f92ca14e1a

---
+++ previews/PR-161/examples/java/Imports.java
@@ -0,0 +1,8 @@
+// Region tags should start after the package, but before imports.
+// [START product_example]
+import com.example.resource;
+
+public class exampleSnippet {
+// Snippet methods ...
+}
+// [END product_example]
